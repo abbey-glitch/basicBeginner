a8c376be1468e6894984688b8382336c4e5eba38
abbey-glitch <abiodunonaolapi@gmail.com>
2023-11-30 14:02:18 +0100

    flex teaching

---
+++ flex/script.js
@@ -0,0 +1,24 @@
+const paymentForm = document.getElementById('tea_form');
+paymentForm.addEventListener("submit", payWithPaystack, false);
+
+function payWithPaystack() {
+    alert('hello')
+  //e.preventDefault();
+
+  let handler = PaystackPop.setup({
+    key: 'pk_test_32d651aa8938dbee2ea372947e24250ade63a645', // Replace with your public key
+    email: document.getElementById("email").value,
+    amount: document.getElementById("amount").value * 100,
+    ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+    // label: "Optional string that replaces customer email"
+    onClose: function(){
+      alert('Window closed.');
+    },
+    callback: function(response){
+      let message = 'Payment complete! Reference: ' + response.reference;
+      alert(message);
+    }
+  });
+
+  handler.openIframe();
+}
